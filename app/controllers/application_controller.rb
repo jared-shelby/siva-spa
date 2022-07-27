0e0823760640cb91caef08e952f268a1f0197c33
Jared Shelby <jared.shelby@yale.edu>
2022-07-27 17:09:53 -0400

    Controller & rendering updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    # ensure views can be rendered (disabled by default since this is an API)
+    include ActionView::Layouts
+    include ActionController::Rendering
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
     def index
-        render plain: "Hi"
+        render :index
     end
 end
