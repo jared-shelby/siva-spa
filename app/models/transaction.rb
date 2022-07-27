e3f047c2b70f73eb0da7c95988d8601cebb1b623
Jared Shelby <jared.shelby@yale.edu>
2022-07-27 15:50:04 -0400

    Database setup

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
     belongs_to :user
+    belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+jared = User.create(name: "Jared Shelby")
+
+ski = Goal.create(name: "Ski Trip", amount: "2000", target: Time.now, image: "https://media.cntraveler.com/photos/5f84939a5f9755e5951db3f4/master/pass/WhitefishMountainResort-3.jpg", user: jared)
+
+food_drink = Category.create(name: "Food/Drink")
+
+starbucks = Transaction.create(amount: "4.56", date: Time.now, merchant: "Starbucks", user: jared, category: food_drink)
