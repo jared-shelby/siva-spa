93f77bf2524b775295c8a64d63111579bd1fb916
Jared Shelby <jared.shelby@yale.edu>
2022-07-27 22:59:56 -0400

    Javascript connected successfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
+# Clear old seed data before re-seeding
 jared = User.create(name: "Jared Shelby")
 
 ski = Goal.create(name: "Ski Trip", amount: "2000", target: Time.now, image: "https://media.cntraveler.com/photos/5f84939a5f9755e5951db3f4/master/pass/WhitefishMountainResort-3.jpg", user: jared)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+console.log("JS connected to HTML successfully")
+
+const content = document.querySelector("#content");
+const homeButton = document.querySelector("#home");
+const goalsButton = document.querySelector("#goals");
+const spendingButton = document.querySelector("#spending");
+
+homeButton.addEventListener("click", event => {
+    content.style.backgroundColor = "red";
+    content.innerHTML = "<h2>Home</h2>"
+})
+
+goalsButton.addEventListener("click", event => {
+    content.style.backgroundColor = "yellow";
+    content.innerHTML = "<h2>Milestones</h2>"
+})
+
+spendingButton.addEventListener("click", event => {
+    content.style.backgroundColor = "blue";
+    content.innerHTML = "<h2>Spending</h2>"
+})
