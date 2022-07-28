b21ddbec4501c1bf87bdc2ca04cb90e067fd20c1
Jared Shelby <jared.shelby@yale.edu>
2022-07-28 15:22:38 -0400

    PATCH /users/:id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user
     end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
