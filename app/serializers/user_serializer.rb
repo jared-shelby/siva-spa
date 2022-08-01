5d89b8c5d20f25e133b6838c70419b5a7d03a724
Jared Shelby <jared.shelby@yale.edu>
2022-08-01 15:14:20 -0400

    serializers updates

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :total_spent
   has_many :goals
   has_many :transactions
+
+  def total_spent
+    object.transactions.sum { |t| t.amount }
+  end
 end
