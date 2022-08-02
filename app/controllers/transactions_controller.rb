88160990ab6620dd493d500e49b1cb300c0c3bd5
Jared Shelby <jared.shelby@yale.edu>
2022-08-02 10:44:21 -0400

    ordered transactions by date

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
     def index
-        transactions = Transaction.all
+        transactions = Transaction.all.order("date DESC")
         render json: transactions
     end
 
