cb4d1f368fb62c7b513fd149786610f7fafbd582
Jared Shelby <jared.shelby@yale.edu>
2022-08-02 22:56:45 -0400

    seed & schema updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
-# Clear old seed data before re-seeding
 jared = User.create(name: "Jared Shelby", email: "jared.shelby@yale.edu")
 
-ski = Goal.create(name: "Ski Trip", amount: 2000, target: Time.now, image: "https://media.cntraveler.com/photos/5f84939a5f9755e5951db3f4/master/pass/WhitefishMountainResort-3.jpg", user: jared)
-home = Goal.create(name: "New House", amount: 40000, target: Time.now, image: "https://media.cntraveler.com/photos/5f84939a5f9755e5951db3f4/master/pass/WhitefishMountainResort-3.jpg", user: jared)
+Goal.create(name: "Ski Trip", amount: 2000, funded: 0, description: "Can't wait to go to the alps and have a great time!", target: Time.now, image: "https://media.cntraveler.com/photos/5f84939a5f9755e5951db3f4/master/pass/WhitefishMountainResort-3.jpg", user: jared)
+Goal.create(name: "New House", amount: 40000, funded: 0, description: "Excited to work towards a down payment for our future forever home <3.", target: Time.now, image: "https://foyr.com/learn/wp-content/uploads/2021/08/design-your-dream-home.jpg", user: jared)
 
 food_drink = Category.create(name: "Food/Drink")
 entertainment = Category.create(name: "Entertainment")
 bills = Category.create(name: "Bills")
 
-starbucks = Transaction.create(amount: 4.56, date: Time.now, merchant: "Starbucks", user: jared, category: food_drink)
-movies = Transaction.create(amount: 15.67, date: Time.now, merchant: "AMC Theaters", user: jared, category: entertainment)
-electric = Transaction.create(amount: 180, date: Time.now, merchant: "ABC Electric", user: jared, category: bills)
-phone = Transaction.create(amount: 50.60, date: Time.now, merchant: "Verizon", user: jared, category: bills)
+Transaction.create(amount: 4.56, date: Time.now, merchant: "Coffee Bean", user: jared, category: food_drink)
+Transaction.create(amount: 15.67, date: Time.now, merchant: "Theater Delight", user: jared, category: entertainment)
+Transaction.create(amount: 180, date: Time.now, merchant: "Zap Electric", user: jared, category: bills)
+Transaction.create(amount: 50.60, date: Time.now, merchant: "Mobile Madness", user: jared, category: bills)
